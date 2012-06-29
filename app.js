9dc7bfbe33c8c0cd9c4fa6333bdf5589380c77a9
Darrell Mozingo <darrell@mozingo.net>
2012-06-29 17:19:07 +0100

    Pulled out a view for the homepage.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var app = require('express').createServer();
 
+app.engine('html', require('ejs').__express);
+app.set('view engine', 'html');
+
 app.get('/', function(req, res) {
-  res.send('Bot Challenge - coming soon');
+	res.render('index', { title: 'Bot Challenge' });
 });
 
 app.listen(8081);
