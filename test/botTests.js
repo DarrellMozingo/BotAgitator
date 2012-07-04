d0c864e446a4980d3a6c352b97373713a1a6c771
Darrell Mozingo <darrell@mozingo.net>
2012-07-04 12:41:29 +0100

    Test refactoring.

--- test/botTests.js
+++ test/botTests.js
@@ -5,15 +5,15 @@ var bot = require('../bot');
 
 describe('Bot', function() {
 	describe('Upload', function() {
+		var fakeRequest = function() {
+			this.body = { 'file_contents' : '' };
+		};
+
 		it('should write the uploaded content to a temporary location and notify the user of success', function() {
 			var fakeResponse = function() {
 				this.send = function(message) {
 					assert.equal(message, 'Received player.');
 				};
-			};
-
-			var fakeRequest = function() {
-				this.body = { 'file_contents' : '' };
 			};
 
 			fs.writeFile = function(file, content, callback) { 
@@ -26,15 +26,11 @@ describe('Bot', function() {
 			new bot().upload(new fakeRequest(), new fakeResponse());
 		})
 		
-		it('should notify the user of errors', function() {
+		it('should notify the user of an error writing the file', function() {
 			var fakeResponse = function() {
 				this.send = function(message) {
 					assert.equal(message, 'error message!');
 				};
-			};
-
-			var fakeRequest = function() {
-				this.body = { 'file_contents' : '' };
 			};
 
 			fs.writeFile = function(file, content, callback) { 
